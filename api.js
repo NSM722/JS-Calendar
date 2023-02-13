5a68f55a9ca03d298e70ddb38ec9af2e046e3e1f
Natalie <nataliembatha@gmail.com>
2023-02-13 15:08:13 +0100

    deleted debugging comments

--- api.js
+++ api.js
@@ -2,27 +2,11 @@ export const getAdvice = () => {
   let output = document.querySelector("#advice");
   const API = "https://api.adviceslip.com/advice"
   fetch(API)
-  .then(response => response.json())
-  .then(data => {
-    output.textContent = data.slip.advice;
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
-
-/*
-
-  try {
-    excusesAPI.then(response => {
-      response.json();
-    })
+    .then(response => response.json())
     .then(data => {
-      excuse.innerText = data.excuse;
+      output.textContent = data.slip.advice;
     })
-  } catch(error) {
+    .catch(error => {
       console.error(error);
-  }
-
-
-*/
+    });
+}
